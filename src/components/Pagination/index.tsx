680b124da1d4c56e1ac4d04cc042c0a996f45027
thales <thalesdaniel2013@gmail.com>
2022-04-27 10:58:09 -0300

    feat: iniciado novo componente de paginação

---
+++ src/components/Pagination/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Pagination() {
+  return (
+    <div>
+      teste
+    </div>
+  );
+}
+
+export default Pagination;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Pagination from '../../components/Pagination';
 import PokemonList from '../../components/PokemonList';
 import './style.css';
 
@@ -6,6 +7,7 @@ function Home() {
   return (
     <div className="home">
       <PokemonList />
+      <Pagination />
     </div>
   );
 }
