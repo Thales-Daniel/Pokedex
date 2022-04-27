dd18c2d987384dd009106cebaf107de3d00614d7
thales <thalesdaniel2013@gmail.com>
2022-04-27 13:17:52 -0300

    style: adicionado style dos botões

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,12 +1,13 @@
 import React, { useContext } from 'react';
 import { PokemonContext } from '../../context/Provider';
+import './style.css';
 
 function Pagination() {
   const { setPagina, pagina } = useContext(PokemonContext);
   return (
     <div>
-      <button type="button" onClick={() => (pagina > 0 ? setPagina(pagina - 10) : setPagina(0))}>Anterior</button>
-      <button type="button" onClick={() => setPagina(pagina + 10)}>Proximo</button>
+      <button className="pageButton" type="button" onClick={() => (pagina > 0 ? setPagina(pagina - 10) : setPagina(0))}>Anterior</button>
+      <button className="pageButton" type="button" onClick={() => setPagina(pagina + 10)}>Proximo</button>
     </div>
   );
 }
